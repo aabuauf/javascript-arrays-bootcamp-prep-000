f6d1dbc86d269fbc62741a9a618640f8dc66e8d6
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-13 01:43:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,8 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,index){
-  var newarray = [array(index),...array];
-  return newarray;
+ 
 }
 function destructivelyAddElementToBeginningOfArray(array,index){
-  return [array(index),...array];
+ 
 }
